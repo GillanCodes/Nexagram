3a1dbf264a2713c64b1944a3ac14d1d38663a92d
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-24 11:02:51 +0200

    changed "full_name" => "fullname"

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -22,7 +22,7 @@ export const register = async(req:Request, res:Response) => {
         if (username.length > 32) throw Error('register_field_too_long_username');
         if (password.length > 255) throw Error('register_field_too_long_password');
 
-        const user = await userModel.create({email, password, username, full_name : fullname});
+        const user = await userModel.create({email, password, username, fullname});
         return res.status(201).json({user:user._id});
     } catch (error) {
         const errors = registerErrors(error);
