a826a5f406a096a91dd3014d4168ed0dd67a6566
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-24 11:35:41 +0200

    Update user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,21 @@
 import { Router } from "express";
 let router:Router = Router();
 
+import {
+    getUser,
+    getUsers,
+    updateUser,
+    changeUsername,
+    changeUserPicture,
+    changeUserSettings
+} from "../controllers/user.controller";
+
+router.get('/:id', getUser);
+router.get('/all', getUsers);
+
+router.patch('/:id/', updateUser);
+router.patch('/:id/username', changeUsername);
+router.patch('/:id/avatar', changeUserPicture);
+router.patch('/:id/settings', changeUserSettings);
+
 export default router;
