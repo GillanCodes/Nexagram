cb76513554a1ba79a3a904a3b32a6b1174f500eb
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-25 18:29:11 +0200

    frontend => profile edit

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -13,14 +13,29 @@ export default function Profile() {
 
     const [state, setState] = useState({
         isLoad: false,
-        isOwner: false
+        isOwner: false,
+        editMode: false,
+        avatarMode: false
     });
+
+    const [userState, setUserState] = useState({
+        username: "",
+        bio: "",
+        fullname: ""
+    });
+
+    const modalHandle = () => {
+        setState(state => ({...state, }))
+    }
 
     useEffect(() => {
         if (!isEmpty(usersData))
             setState(state => ({...state, isLoad: true}));
         if (!isEmpty(userData) && userData.username === username)
+        {
             setState(state => ({...state, isOwner: true}));
+            setUserState(state => ({...state, username:userData.username, bio:userData.bio, fullname:userData.fullname}));
+        }
     }, [usersData, userData]);
 
     return (
@@ -31,13 +46,22 @@ export default function Profile() {
                         <div className="profile-page">
                             <div className="profile-header">
                                 <div className="avatar">
-                                    <img src={`${process.env.REACT_APP_CDN_URL}/profile/${user.avatar}`} alt="avatar" />
+                                    <img src={`${process.env.REACT_APP_CDN_URL}/profile/${user.avatar}`} alt="avatar" onClick={() => state.isOwner && modalHandle} />
                                 </div>
                                 <div className="infos">
                                     <div className="user">
-                                        <h2>{user.username}</h2>
+                                        {state.editMode ? (
+                                            <input type="text" value={userState.username} onChange={(e) => setUserState(userState => ({...userState, username:e.target.value}))} className="input" />
+                                        ) : (
+                                            <h2>{userState.username}</h2>
+                                        )}
+                                        {state.editMode ? (
+                                            <input type="text" value={userState.fullname} onChange={(e) => setUserState(userState => ({...userState, fullname: e.target.value}))} className="input" />
+                                        ) : (
+                                            <h3>{userState.fullname}</h3>
+                                        )}
                                         <button className='button'>Follow</button>
-                                        {state.isOwner && (<button className='button'>Edit</button>)}
+                                        {state.isOwner && (<button className='button' onClick={() => setState(state => ({...state, editMode:!state.editMode}))}>Edit</button>)}
                                     </div>
                                     <div className="stats">
                                         <div className="stats-content">
@@ -46,7 +70,11 @@ export default function Profile() {
                                         </div> 
                                     </div>
                                     <div className="bio">
-                                        <p>{user.bio}</p>
+                                        {state.editMode ? (
+                                            <textarea style={{resize: "none"}} name="bio" cols={60} rows={5} value={userState.bio} onChange={(e) => setUserState(userState => ({...userState, bio:e.target.value}))}></textarea>
+                                        ) : (
+                                            <p>{userState.bio}</p>
+                                        )}
                                     </div>
                                 </div> 
                             </div>
