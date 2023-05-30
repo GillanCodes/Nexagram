1e2030bef17cb3f1c96e01b6914db17e3cc241e1
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-05-30 11:11:25 +0200

    added like/unlike actions

--- client/src/actions/posts.action.ts
+++ client/src/actions/posts.action.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const GET_ALL_POSTS  = "GET_ALL_POSTS";
-export const CREATE_POST    = "CREATE_POST";
+export const GET_ALL_POSTS:string   = "GET_ALL_POSTS";
+export const CREATE_POST:string     = "CREATE_POST";
+export const LIKE_POST:string       = "LIKE_POST";
+export const UNLIKE_POST:string     = "UNLIKE_POST";
 
 export const getAllPosts = () => {
     return(dispatch:any) => {
@@ -26,10 +28,37 @@ export const createPost = (data:any) => {
             url: `${process.env.REACT_APP_API_URL}/post/`,
             data
         }).then((res) => {
-            console.log("OUI", res.data);
             dispatch({type:CREATE_POST, payload: res.data});
         }).catch((err) => {
             console.log("NON", err);
         })
+    }
+}
+
+export const likePost = (id:string) => {
+    return(dispatch:any) => {
+        return axios({
+            method: "PATCH",
+            withCredentials: true,
+            url: `${process.env.REACT_APP_API_URL}/post/${id}/like`
+        }).then((res) => {
+            dispatch({type: LIKE_POST});
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+}
+
+export const unlikePost = (id:string) => {
+    return(dispatch:any) => {
+        return axios({
+            method: "PATCH",
+            withCredentials: true,
+            url: `${process.env.REACT_APP_API_URL}/post/${id}/unlike`
+        }).then((res) => {
+            dispatch({type: UNLIKE_POST});
+        }).catch((err) => {
+            console.log(err);
+        });
     }
 }
