8cdbabc36aded404f97d75157b8c49e53162ce9f
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-06-06 15:58:25 +0200

    login register errors done.

--- client/src/components/Auth/Login.tsx
+++ client/src/components/Auth/Login.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { isEmpty } from '../../Utils';
 
 export default function Login() {
 
+    const [errors, setErrors]:[errors:any, setErrors:any] = useState();
     const [state, setState] = useState({email: "", password: ""});
 
     const submitHandle = (e:React.FormEvent) => {
@@ -17,9 +19,10 @@ export default function Login() {
                 password: state.password
             }
         }).then((res) => {
-            if (res.data.errors) return console.log("err");
-            const win:Window = window;
-            win.location = '/';
+            if (res.data.errors) return setErrors(res.data.errors);
+            console.log(res.data);
+            // const win:Window = window;
+            // win.location = '/';
         }).catch((err) => {
             console.log(err);
         });
@@ -29,7 +32,9 @@ export default function Login() {
         <div className='auth-container'>
             <div className="content">
                 <form onSubmit={(e) => submitHandle(e)}>
+                    <p className='error'>{!isEmpty(errors) && errors.log}</p>
                     <input className="input" type="email" name="email" id="email" placeholder='Email' onChange={(e) => setState({...state, email:e.target.value})} />
+                    <p className='error'>{!isEmpty(errors) && errors.password}</p>
                     <input className="input" type="password" name="password" id="password" placeholder='Password' onChange={(e) => setState({...state, password:e.target.value})} />
                     <input className="button" type="submit" value="Login!" />
                 </form>
