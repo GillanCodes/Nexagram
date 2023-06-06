ef8b9583cfe39dbb0d462b23811c95524bb41fec
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-06-06 15:58:17 +0200

    Register errors display done

--- client/src/components/Auth/Register.tsx
+++ client/src/components/Auth/Register.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { isEmpty } from '../../Utils';
 
 export default function Register() {
 
@@ -10,6 +11,8 @@ export default function Register() {
     fullname: ""
   });
   
+  const [errors, setErrors]:[errors:any, setErrors:any] = useState()
+
   const submitHandle = (e:React.FormEvent) => {
         e.preventDefault();
 
@@ -24,7 +27,7 @@ export default function Register() {
               password: state.password
             }
         }).then((res) => {
-            if (res.data.errors) return console.log("err");
+            if (res.data.errors) return setErrors(res.data.errors);
             const win:Window = window;
             win.location = '/auth';
         }).catch((err) => {
@@ -36,9 +39,13 @@ export default function Register() {
     <div className='auth-container'>
       <div className="content">
         <form onSubmit={submitHandle}>
+          <p className='error'>{!isEmpty(errors) && errors.email}</p>
           <input className="input" type="email" name="email" id="email" placeholder='Email' onChange={(e) => setState(state => ({...state, email: e.target.value}))} />
+          <p className='error'>{!isEmpty(errors) && errors.username}</p>
           <input className="input" type="text" name="username" id="username" placeholder='Username' onChange={(e) => setState(state => ({...state, username: e.target.value}))} />
+          <p className='error'>{!isEmpty(errors) && errors.fullname}</p>
           <input className="input" type="text" name="fullname" id="fullname" placeholder='Full Name' onChange={(e) => setState(state => ({...state, fullname: e.target.value}))} />
+          <p className='error'>{!isEmpty(errors) && errors.password}</p>
           <input className="input" type="password" name="password" id="password" placeholder='Password' onChange={(e) => setState(state => ({...state, password: e.target.value}))} />
           <input type="submit" value="Register" className='button' />
         </form>
