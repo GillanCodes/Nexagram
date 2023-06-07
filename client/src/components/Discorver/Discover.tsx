77c4bce50e50f3bcf5b912d6fab07098a0001799
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-06-07 13:13:17 +0200

    discover page base done.

--- client/src/components/Discorver/Discover.tsx
+++ client/src/components/Discorver/Discover.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { convertDatetoTime, isEmpty } from '../../Utils';
 
 export default function Discover() {
-  return (
-    <div>Discover</div>
-  )
+
+    const postsData = useSelector((state:any) => state.postsReducer);
+
+    const [state, setState] = useState({
+        isLoad: false
+    });
+
+    useEffect(() => {
+        if(!isEmpty(postsData))
+            setState((state) => ({...state, isLoad: true}));
+    }, [postsData])
+
+    return (
+        <div className='discover-container'>
+            <div className="discover-content">
+                {state.isLoad ? (
+                    <>
+                        {postsData.sort((a:any,b:any) => convertDatetoTime(b.createdAt) - convertDatetoTime(a.createdAt)).map((post:any, key:number) => {
+                            return (
+                                <div className="post" id={post._id} key={key}>
+                                    <img src={`${process.env.REACT_APP_CDN_URL}/posts/${post.medias[0]}`} alt="Post-img" />
+                                </div>
+                            )
+                        })} 
+                    </>
+                ): (
+                    <p>Loading</p>
+                )} 
+            </div>
+        </div>
+    )
 }
