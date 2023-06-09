2a2151899d60afbbffbef7d224d3a14ec777078c
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-06-09 09:30:56 +0200

    Fix suggestion of own account

--- client/src/components/Discorver/Suggest.tsx
+++ client/src/components/Discorver/Suggest.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
-import { convertDatetoTime, isEmpty } from '../../Utils';
+import { isEmpty } from '../../Utils';
+import FollowButton from '../Profile/FollowButton';
+
+var users:any = [];
 
 export default function Suggest() {
 
@@ -8,8 +11,6 @@ export default function Suggest() {
     const userData = useSelector((state:any) => state.userReducer);
 
     const [load, setLoad] = useState(false);
-
-    var users:Array<object> = [];
 
     useEffect(() => {
         for (let index = 0; index < 3; index++) {
@@ -19,11 +20,10 @@ export default function Suggest() {
                 else rdm++
             users.push(usersData[rdm]);
         }
-        console.log(users)
     }, [usersData]);
 
     useEffect(() => {
-        if (!isEmpty(users) && users.length === 3)
+        if (!isEmpty(users))
             setLoad(true);
     }, [users])
 
@@ -31,13 +31,13 @@ export default function Suggest() {
         <div className='suggest'>
             {load && (
                 <>
-                    {usersData.sort((a:any,b:any) => convertDatetoTime(b.updatedAt) - convertDatetoTime(a.updatedAt)).slice(0,3).map((user:any) => {
+                    {users.map((user:any) => {
                         return (
                             <div className="box">
                                 <img src={`${process.env.REACT_APP_CDN_URL}/profile/${user.avatar}`} alt="avatar" className="avatar" />
                                 <p className='username'>{user.username}</p>
                                 <p>{user.fullname}</p>
-                                <button className='button'>Follow !</button>
+                                <FollowButton userData={userData} usersData={user} />
                             </div>
                         )
                     })}
