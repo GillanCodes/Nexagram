b65c97f467a4bc14a6f877edf88b163f6c1c884d
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-06-14 10:09:15 +0200

    Update socket.ts

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,4 +2,24 @@ const socket = require('socket.io');
 
 module.exports.socket = (httpServer:any) => {
 
+    const whiteList:Array<string | undefined> = [process.env.CLIENT_URL, undefined];
+
+    const io = socket(httpServer, {
+        cors: {
+            origin: function (origin:any, callback:any) {
+                if (whiteList.indexOf(origin) !== -1) {
+                    callback(null, true)
+                } else {
+                    callback(new Error('Not allowed by CORS'))
+                }
+            },
+            methods: ["GET", "POST"]
+        },
+    });
+
+    io.on("connection", (socket:any) => {
+
+    });
+ 
+
 }
