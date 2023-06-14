44318b72e49626173378e419a02ea128561ba20a
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-06-14 10:09:06 +0200

    Message model added

---
+++ models/message.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, model, mongo } from "mongoose";
+
+export interface IMessage 
+{
+    message:string,
+    users:Array<string>,
+    sender:mongoose.Schema.Types.ObjectId,
+    
+};
+
+const messageSchema = new Schema<IMessage>({
+    message: {type:String, required: true},
+    users: Array,
+    sender:{ type: mongoose.Schema.Types.ObjectId, required: true}
+});
+
+const messageModel = model<IMessage>("notification", messageSchema);
+export default messageModel;
