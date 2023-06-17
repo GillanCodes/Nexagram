d358fa27123a5d025f672f124d57ae8aeb284856
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-06-17 10:14:58 +0200

    Update socket.ts

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,11 +1,34 @@
-import messageModel from "../../models/message.model";
+import chatModel from "../../models/chat.model";
 
 const socket = require('socket.io');
 
-interface IData {
-    message:String,
+interface INewChat {
+    owner: String,
     users:Array<string>,
-    sender:string
+    name: String
+}
+
+interface INewMessage
+{
+    chatId:string,
+    senderId:string,
+    content:string
+}
+
+interface IMessage
+{
+    id: string,
+    sender: string,
+    content:string,
+}
+
+interface IChat
+{
+    id: string,
+    owner: string,
+    users: [string],
+    name: string,
+    messages: [IMessage]
 }
 
 module.exports.socket = (httpServer:any) => {
@@ -28,18 +51,36 @@ module.exports.socket = (httpServer:any) => {
     io.on("connection", (socket:any) => {
         io.on('get-chat', (chatId:any) => {
             socket.join(chatId);
-        })
+        });
 
 
-        socket.on('new-message', (data:IData) => {
-            messageModel.create({
-                message:data.message,
-                users:data.users,
-                sender:data.sender
+        socket.on('new-chat', (data:INewChat) => {
+            chatModel.create({
+                owner: data.owner,
+                users: data.users,
+                name: data.name
             }).then((res) => {
-                io.emit('new-message-recived', res);
+                io.emit('new-chat-created', res);
             });
         });
+
+        socket.on('new-message', (data:INewMessage) => {
+            chatModel.findByIdAndUpdate(data.chatId, {
+                $push: {
+                    messages: {
+                        sender: data.senderId,
+                        content: data.content
+                    }
+                }
+            }).then((res) => {
+                io.emit('new-message-emit', res);
+            });
+        });
+
+        socket.on('get-chat', async (data:IChat) => {
+            const chat = await chatModel.findById(data.id);
+            io.emit('send-chat', chat)
+        })
 
     });
  
